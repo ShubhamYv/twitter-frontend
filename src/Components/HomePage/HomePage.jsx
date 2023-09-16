dc902c453ce8e8a2da4c10b3b0f556179f60d046
Shubham <shubhamyadav32100@gmail.com>
2023-09-16 20:00:02 +0530

    Profile part completed

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -3,6 +3,9 @@ import React from 'react'
 import Navigation from '../Navigation/Navigation'
 import HomeSection from '../HomeSection/HomeSection'
 import RightPart from '../RightPart/RightPart'
+import { Route, Routes } from 'react-router-dom'
+import Profile from '../Profile/Profile'
+import TweetDetails from '../TweetDetails/TweetDetails'
 
 const HomePage = () => {
   return (
@@ -11,7 +14,12 @@ const HomePage = () => {
         <Navigation />
       </Grid>
       <Grid item xs={12} lg={6} className='px-5 lg:px-10 hidden lg:block w-full relative'>
-        <HomeSection />
+        <Routes>
+          <Route path='/' element={<HomeSection />} />
+          <Route path='/home' element={<HomeSection />} />
+          <Route path='/profile/:id' element={<Profile />} />
+          <Route path='/tweet/:id' element={<TweetDetails />} />
+        </Routes>
       </Grid>
 
       <Grid item xs={0} lg={3} className='hidden lg:block w-full relative'>
--- src/Components/HomeSection/TweetCard.jsx
+++ src/Components/HomeSection/TweetCard.jsx
@@ -92,7 +92,7 @@ const TweetCard = () => {
           </div>
 
           <div className="mt-2">
-            <div className="cursor-pointer">
+            <div onClick={() => navigate(`/tweet/${3}`)} className="cursor-pointer">
               <p className='mb-2 p-0'>Hello my name is shubham yadav. I am a java full stack developer.</p>
               <img
                 className='w-[28rem] border border-gray-400 p-5 rounded-md'
